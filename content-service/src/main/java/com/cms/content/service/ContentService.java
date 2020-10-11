b1a9d04fa2ecd7752a1591bca51b7d68c2f63408
Giovel Ranoa <gioran0912@gmail.com>
2020-10-11 22:54:14 +0800

    fix: moved utilities package to main

--- content-service/src/main/java/com/cms/content/service/ContentService.java
+++ content-service/src/main/java/com/cms/content/service/ContentService.java
@@ -10,7 +10,7 @@ import com.cms.content.rest.response.ResponseBuilder;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import utilities.Constants;
+import com.cms.content.utilities.Constants;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
